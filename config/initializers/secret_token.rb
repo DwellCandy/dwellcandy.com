bd175d7ff6c5a8b3752deb348cf22570ae79f693
Jimmy <jimwong1023@gmail.com>
2013-11-05 15:19:07 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DwellcandyCom::Application.config.secret_key_base = 'dd8e3c5bed663cb70d7120048bf4dcbc5712d5219d76f853bdf5378cf5e7c44c608c4cb9c5d0bb233946f1529c1e5f4d85b0297adcdf81388e6ed2b92a37f9c1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DwellcandyCom::Application.config.session_store :cookie_store, key: '_dwellcandy_com_session'
